6aeec526fa701f734d60f41c2f204f1978923de5
Guillaume Aubin <aubinguillaume4@gmail.com>
2021-11-09 20:43:33 +0100

    import jwt et cookies auth

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const userRoutes = require('./routes/user.routes');
-require('dotenv').config({path: './config/.env'});
-require('./config/db');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+const { checkUser } = require("./middleware/auth.middleware");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// jwt
+app.get("*", checkUser);
 
 // routes
-app.use('/api/user', userRoutes);
-
+app.use("/api/user", userRoutes);
 
 // server
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-})
+  console.log(`Listening on port ${process.env.PORT}`);
+});
